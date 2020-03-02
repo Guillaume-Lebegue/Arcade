fbc01c3dff782fa09278dad7b33f45c263a1cb22
Quentin <QDM@localhost.localdomain>
2020-03-02 16:58:10 +0100

    [ADD] core/include/: adding the core's header+enums file

---
+++ core/include/Arcade.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_arcade_2019
+** File description:
+** core include
+*/
+
+#include <iostream>
+
+typedef enum {
+    ERR_INVALID_ARGS,
+    ERR_INVALID_INITIAL,
+} error_id_t;
+
+typedef enum {
+    SUCCESS = 0,
+    FAILURE = 84,
+} return_values_t;
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+int error(error_id_t id);
