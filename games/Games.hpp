18b2725420335538bc581077e6d65143bd14fd9c
Quentin <QDM@localhost.localdomain>
2020-03-02 18:18:24 +0100

    [ADD] games: adding games' and lib's Makefile

---
+++ games/Games.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_arcade_2019
+** File description:
+** games include
+*/
+
+typedef enum {
+    PREVGLIB = 'l',
+    NEXTGLIB = 'm',
+    PREVGAME = 'o',
+    NEXTGAME = 'p',
+    RESTART  = 'i',
+    TOMENU   ='\t',
+    EXIT     = 27,  /* ESC */
+} keypress_t;
